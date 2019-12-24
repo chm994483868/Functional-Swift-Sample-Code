ffbaf2e9f1bf1b6f44fe160836987b6a1b1256ab
ChengHeMing <994483868@qq.com>
2019-12-25 06:47:44 +0800

    Region
    1，添加区域操作，用已有的区域构建新的区域。

--- Battleship/Battleship/Battleship.swift
+++ Battleship/Battleship/Battleship.swift
@@ -85,10 +85,11 @@ func shift(region: @escaping Region, offset: Position) -> Region {
 }
 
 // 表示圆心是 (5, 5) 半径为 10 的圆
+// 首先传入参数 circle(10) 表示圆心在原点半径为 10 的圆， 后面其实是任意一点与 (5, 5) 的距离，与 10 这个半径做比较。
 let temp = shift(region: circle(10), offset: Position(x: 5, y: 5))
 
 // 由原区域以外的所有点组成
-func invert(region: @escaping Region) -> Region {
+func invert(_ region: @escaping Region) -> Region {
     return { point in !region(point) }
 }
 
@@ -96,3 +97,21 @@ func invert(region: @escaping Region) -> Region {
 //let isCircle = temp(Position(x: 3, y: 2))
 // 判断 x: 3 y: 2 的 Position 是否在原点是圆心半径是 5 的圆内
 let isCircle = circle(5)(Position(x: 3, y: 2))
+
+// 两个区域的交集（用 && 连接）
+func intersection(_ region1: @escaping Region, _ regior2: @escaping Region) -> Region {
+    return { point in region1(point) && regior2(point) }
+}
+
+// 两个区域的并集（用 || 连接）
+func union(_ region1: @escaping Region, _ region2: @escaping Region) -> Region {
+    return { point in region1(point) || region2(point) }
+}
+
+/// 所有在第一个区域中且不在第二个区域中的点构建一个新的区域
+/// - Parameters:
+///   - region: 第一个区域
+///   - minus: 要被剪去的第二个区域
+func difference(_ region: @escaping Region, _ minus: @escaping Region) -> Region {
+    return intersection(region, invert(minus))
+}
