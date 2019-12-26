5ce3c22bf8e93de1d803354a5b7376473700a5a4
ChengHeMing <994483868@qq.com>
2019-12-26 22:58:33 +0800

    添加 colorGenerator 方法。

--- EncapsulationCoreImage/EncapsulationCoreImage/EncapsulationCoreImage.swift
+++ EncapsulationCoreImage/EncapsulationCoreImage/EncapsulationCoreImage.swift
@@ -7,17 +7,30 @@
 //
 
 import UIKit
+import Foundation
 import CoreImage
 
-typealias Filter = CIImage -> CIImage
+typealias Filter = (CIImage) -> CIImage
 
 func blur(_ radius: Double) -> Filter {
     return { image in
         let parameters = [kCIInputRadiusKey: radius,
-                          kCIInputImageKey: image]
+                          kCIInputImageKey: image] as [String : Any]
         
         guard let filter = CIFilter(name: "CIGaussianBlur", parameters: parameters) else { fatalError("CIFilter(name:, parameters:) 执行错误") }
         guard let outputImage = filter.outputImage else { fatalError("filter.outputImage 为 nil") }
+        
+        return outputImage
+    }
+}
+
+func colorGenerator(color: NSColor) -> Filter {
+    return { _ in
+        guard let c = CIColor(color: color) else { fatalError("Failed to get `CIColor`") }
+        let parameters = [kCIInputColorKey: c]
+        
+        guard let filter = CIFilter(name: "CIConstantColorGenerator", parameters: parameters) else { fatalError("`CIFilter` creation failed") }
+        guard let outputImage = filter.outputImage else { fatalError("Failed to get `outputImage`") }
         
         return outputImage
     }
