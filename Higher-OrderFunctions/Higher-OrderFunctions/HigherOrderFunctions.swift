10fbd3f8bd63a4ffea694640800148439fd98f32
chengheming <chengheming@yingmob.com>
2019-12-31 11:38:41 +0800

    1. Filter，Reduce。

--- Higher-OrderFunctions/Higher-OrderFunctions/HigherOrderFunctions.swift
+++ Higher-OrderFunctions/Higher-OrderFunctions/HigherOrderFunctions.swift
@@ -87,3 +87,124 @@ extension Array {
         return result
     }
 }
+
+// Filter
+
+let exampleFiles = ["README.md", "HelloWorld.swift", "FlappyBird.swift"]
+
+func getSwiftFiles(_ files: [String]) -> [String] {
+    var result: [String] = []
+    for file in files {
+        if file.hasSuffix(".swift") {
+            result.append(file)
+        }
+    }
+    
+    return result
+}
+
+//_ = getSwiftFiles(exampleFiles)
+
+extension Array {
+    func filter(_ includeElement: (Element) -> Bool) -> [Element] {
+        var result: [Element] = []
+        for x in self where includeElement(x) {
+            result.append(x)
+        }
+        
+        return result
+    }
+}
+
+func getSwiftFiles2(_ files: [String]) -> [String] {
+    return files.filter { file in
+        file.hasSuffix(".swift")
+    }
+}
+                                                                                                        
+// Reduce
+
+func sum(_ xs: [Int]) -> Int { // 使用 Int 可能会很容易溢出
+    var result: Int = 0
+    for  x in xs {
+        result += x
+    }
+    return result
+}
+
+func product(_ xs: [Int]) -> Int {
+    var result: Int = 1
+    for x in xs {
+        result = x * result
+    }
+    return result
+}
+
+func concatenate(_ xs: [String]) -> String {
+    var result: String = ""
+    for x in xs {
+        result += x
+    }
+    return result
+}
+
+func prettyPrintArray(_ xs: [String]) -> String {
+    var result: String = "Entries in the array xs: \n"
+    for x in xs {
+        result = " " + result + x + "\n"
+    }
+    return result
+}
+
+extension Array {
+    func reduce<T>(initial: T, combine: (T, Element) -> T) -> T {
+        var result = initial
+        for x in self {
+            result = combine(result, x)
+        }
+        
+        return result
+    }
+}
+
+func sumUsingReduce(_ xs: [Int]) -> Int {
+    return xs.reduce(initial: 0) { result, x in
+        result + x
+    }
+}
+
+func productUsingReduce(xs: [Int]) -> Int {
+    return xs.reduce(initial: 1, combine: *)
+}
+
+func concatUsingReduce(xs: [String]) -> String {
+    return xs.reduce(initial: "", combine: +)
+}
+
+func flatten<T>(xss: [[T]]) -> [T] {
+    var result: [T] = []
+    for xs in xss {
+        result += xs
+    }
+    return result
+}
+
+func flattenUsingReduce<T>(xss: [[T]]) -> [T] {
+    return xss.reduce(initial: []) { result, xs in
+        result + xs
+    }
+}
+
+extension Array {
+    func mapUsingReduce<T>(transform: (Element) -> T) -> [T] {
+        return reduce(initial: []) { result, x in
+            return result + [transform(x)]
+        }
+    }
+    
+    func filterUsingReduce(includeElement: (Element) -> Bool) -> [Element] {
+        return reduce(initial: []) { result, x in
+            return includeElement(x) ? result + [x] : result
+        }
+    }
+}
