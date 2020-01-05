1ce296a72695642730c41cf035d5b2112ffa17ca
ChengHeMing <994483868@qq.com>
2020-01-05 22:08:44 +0800

    1. 完善 QuickCheck.

--- QuickCheck/QuickCheck/QuickCheck.swift
+++ QuickCheck/QuickCheck/QuickCheck.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 func plusIsCommutative(_ x: Int, _ y: Int) -> Bool {
     return x + y == y + x
@@ -26,10 +27,74 @@ extension Int: Arbitrary {
     }
 }
 
+extension CGFloat: Arbitrary {
+    static func arbitrary() -> CGFloat {
+        return CGFloat(arc4random())
+    }
+}
+
 let temporary = Int.arbitrary()
 
 extension Character: Arbitrary {
     static func arbitrary() -> Character {
-        return UnicodeScalar(Int.random(form: 65, to: 90))
+        return Character(UnicodeScalar(Int.random(from: 65, to: 90))!) // 大写字母 A-Z 的 ASCII 码
     }
 }
+
+func tabulate<A>(times: Int, transform: (Int) -> A) -> [A] {
+    return (0..<times).map(transform)
+}
+
+extension Int {
+    static func random(from: Int, to: Int) -> Int {
+        return from + (Int(arc4random()) % (to - from))
+    }
+}
+
+extension String: Arbitrary {
+    static func arbitrary() -> String {
+        let randomLength = Int.random(from: 0, to: 40) // 随机生成一个 0 - 40 之间的数字，接着随机生成一个该长度的字符串，字符串的每个字母为 A - Z 之间的随机一个
+        let randomCharacter = tabulate(times: randomLength) { _ in
+            Character.arbitrary()
+        }
+        
+        return String(randomCharacter)
+    }
+}
+
+let temporaryString = String.arbitrary()
+let numberOfIterations = 100
+
+func check1<A: Arbitrary>(message: String, _ property: (A) -> Bool) {
+    for _ in 0..<numberOfIterations {
+        let value = A.arbitrary()
+        guard property(value) else {
+            print("\"\(message)\" doesn't hold: \(value)")
+            return
+        }
+        print("\"\(message)\" passed \(numberOfIterations) tests.")
+    }
+}
+
+extension CGSize {
+    var area: CGFloat {
+        return width * height
+    }
+}
+
+extension CGSize: Arbitrary {
+    static func arbitrary() -> CGSize {
+        return CGSize(width: CGFloat.arbitrary(), height: CGFloat.arbitrary())
+    }
+}
+
+//check1(message: "Area should be at least 0") { (size: CGSize) in
+//    size.area >= 0
+//}
+
+//check1(message: "Evary string starts with Hello") { (s: String) -> Bool in
+//    s.hasPrefix("Hello")
+//}
+
+
+
