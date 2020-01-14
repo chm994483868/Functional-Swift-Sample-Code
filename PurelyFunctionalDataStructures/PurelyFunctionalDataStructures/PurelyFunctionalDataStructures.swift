f1138b725adcf643e6fe090466ac50bbb85eda1f
chengheming <chengheming@yingmob.com>
2020-01-14 15:14:53 +0800

    1. 完善 Trie。

--- PurelyFunctionalDataStructures/PurelyFunctionalDataStructures/PurelyFunctionalDataStructures.swift
+++ PurelyFunctionalDataStructures/PurelyFunctionalDataStructures/PurelyFunctionalDataStructures.swift
@@ -27,9 +27,10 @@ func contains<Element: Equatable>(x: Element, _ set: [Element]) -> Bool {
 }
 
 func insert<Element: Equatable>(x: Element, _ set: [Element]) -> [Element] {
-    return contains(x: x, set) ? set : set + [x]
+    return contains(x: x, set) ? set : [x] + set
 }
 
+// 支持递归的枚举
 indirect enum BinarySearchTree<Element: Comparable> {
     case Leaf
     case Node(BinarySearchTree<Element>, Element, BinarySearchTree<Element>)
@@ -57,9 +58,7 @@ extension BinarySearchTree {
             return 1 + left.count + right.count
         }
     }
-}
-
-extension BinarySearchTree {
+    
     var elements: [Element] {
         switch self {
         case .Leaf:
@@ -68,9 +67,7 @@ extension BinarySearchTree {
             return left.elements + [x] + right.elements
         }
     }
-}
-
-extension BinarySearchTree {
+    
     var isEmpty: Bool {
         if case .Leaf = self {
             return true
@@ -79,28 +76,29 @@ extension BinarySearchTree {
     }
 }
 
-//extension BinarySearchTree where Element: Comparable {
-//    var isBST: Bool {
-//        switch self {
-//        case .Leaf:
-//            return true
-//        case let .Node(left, x, right):
-//            return left.elements.all {y in y < x }
-//                && right.elements.all { y in y > x }
-//                && left.isBST
-//                && right.isBST
-//        }
-//    }
-//}
+extension BinarySearchTree where Element: Comparable {
+    var isBST: Bool {
+        switch self {
+        case .Leaf:
+            return true
+        case let .Node(left, x, right):
+            return left.elements.all { $0 < x }
+                && right.elements.all { $0 > x }
+                && left.isBST
+                && right.isBST
+        }
+    }
+}
 
-//extension Sequence {
-//    func all(predicate: (Generator.Element) -> Bool) -> Bool {
-//        for x in self where !predicate(x) {
-//            return false
-//        }
-//        return true
-//    }
-//}
+extension Sequence {
+    func all(predicate: (Iterator.Element) -> Bool) -> Bool {
+        for x in self where !predicate(x) {
+            return false
+        }
+        return true
+    }
+    
+}
 
 extension BinarySearchTree {
     func contains(x: Element) -> Bool {
@@ -117,9 +115,7 @@ extension BinarySearchTree {
             fatalError("The impossible occurred")
         }
     }
-}
-
-extension BinarySearchTree {
+    
     mutating func insert(x: Element) {
         switch self {
         case .Leaf:
@@ -133,9 +129,9 @@ extension BinarySearchTree {
 }
 
 //let myTree: BinarySearchTree<Int> = BinarySearchTree()
-//var copid = myTree
-//copid.insert(x: 5)
-//(myTree.elements, copid.elements) // ([], [5])
+//var copied = myTree
+//copied.insert(x: 5)
+//(myTree.elements, copied.elements)
 
 func autocomplete(history: [String], textEntered: String) -> [String] {
     return history.filter { $0.hasPrefix(textEntered) }
@@ -144,18 +140,19 @@ func autocomplete(history: [String], textEntered: String) -> [String] {
 struct Trie<Element: Hashable> {
     let isElement: Bool
     let children: [Element: Trie<Element>]
-}
-
-extension Trie {
+    
     init() {
         isElement = false
         children = [:]
     }
-}
-
-extension Trie {
+    
+    init(isElement: Bool, children: [Element: Trie<Element>]) {
+        self.isElement = isElement
+        self.children = children
+    }
+    
     var elements: [[Element]] {
-        var result: [[Element]] = isElement ? [[]]: []
+        var result: [[Element]] = isElement ? [[]] : []
         for (key, value) in children {
             result += value.elements.map { [key] + $0 }
         }
@@ -165,6 +162,65 @@ extension Trie {
 
 extension Array {
     var decompose: (Element, [Element])? {
-        return (isEmpty ? nil : (self[startIndex], Array(self.dropFirst())))
+        return isEmpty ? nil : (self[startIndex], Array(self.dropFirst()))
+    }
+}
+
+// 递归求和
+func sum(xs: [Int]) -> Int {
+    guard let (head, tail) = xs.decompose else {
+        return 0
+    }
+    return head + sum(xs: tail)
+}
+
+/// 快速排序
+/// - Parameter input:
+func qsort(input: [Int]) -> [Int] {
+    guard let (pivot, rest) = input.decompose else {
+        return []
+    }
+    
+    let lesser = rest.filter { $0 < pivot } // 统计小于第一个元素的所有元素
+    let greater = rest.filter { $0 >= pivot } // 统计大于等于第一个元素的所有元素
+    
+    return qsort(input: lesser) + [pivot] + qsort(input: greater) // 拼接
+}
+
+extension Trie {
+    func lookup(key: [Element]) -> Bool {
+        guard let (head, tail) = key.decompose else { return isElement } // 如果 key 是空数组，返回 Trie 实例的 isElement
+        guard let subtrie = children[head] else { return false }
+        return subtrie.lookup(key: tail)
+    }
+    
+    func withPrefix(prefix: [Element]) -> Trie<Element>? {
+        guard let (head, tail) = prefix.decompose else { return self } // 如果 key 是空数组，返回 Trie 实例
+        guard let remainder = children[head] else { return nil }
+        return remainder.withPrefix(prefix: tail)
+    }
+    
+    func autocomplete(key: [Element]) -> [[Element]] {
+        return withPrefix(prefix: key)?.elements ?? []
+    }
+    
+    init(_ key: [Element]) {
+        if let (head, tail) = key.decompose {
+            let children = [head: Trie(tail)]
+            self = Trie(isElement: false, children: children)
+        } else {
+            self = Trie(isElement: true, children: [:])
+        }
+    }
+    
+    func insert(key: [Element]) -> Trie<Element> {
+        guard let (head, tail) = key.decompose else { return Trie(isElement: true, children: children) }
+        var newChildren = children
+        if let nextTrie = children[head] {
+            newChildren[head] = nextTrie.insert(key: tail)
+        } else {
+            newChildren = Trie(tail)
+        }
+        return Trie(isElement: isElement, children: newChildren)
     }
 }
