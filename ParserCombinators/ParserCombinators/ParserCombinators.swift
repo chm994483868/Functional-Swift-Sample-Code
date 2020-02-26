2a57d6e41c32e2c7f8deeb7f7077f510b04816ba
ChengHeMing <994483868@qq.com>
2020-02-26 22:50:01 +0800

    1. ParserCombinators。

--- ParserCombinators/ParserCombinators/ParserCombinators.swift
+++ ParserCombinators/ParserCombinators/ParserCombinators.swift
@@ -9,14 +9,46 @@
 import Foundation
 
 struct Parser<Token, Result> {
-    let p: ArraySlice<Token> -> AnySequence<(Result, ArraySlice<Token>)>
+    let p: (ArraySlice<Token>) -> AnySequence<(Result, ArraySlice<Token>)>
+    //
+    //    func parserA() -> Parser<Character, Character> {
+    //
+    //    }
+    //
+        func one<A>(x: A) -> AnySequence<A> {
+            return AnySequence(GeneratorOfOne(x))
+        }
     
-    func parserA() -> Parser<Character, Character> {
-        
+    func none<T>() -> AnySequence<T> {
+        return AnySequence(anyGenerator { nil } )
     }
     
-    func one<A>(x: A) -> AnySequence<A> {
-        return AnySequence(GeneratorOfOne(x))
+    func parseA() -> Parser<Character, Character> {
+        let a: Character = "a"
+        
+        //        return Parser { x in
+        //            guard let (head, tail) = x.decompose, head == a else {
+        //                return none()
+        //            }
+        //
+        //            return one((a, tail))
+        //            } as! Parser<Character, Character>
+        
+        return Parser { (x) -> AnySequence<(Result, ArraySlice<Token>)> in
+            guard let (head, tail) = x.decompose, head == a else {
+                return self.none()
+            }
+            
+            return one(x: (a, tail))
+        }
     }
 }
     
+
+func testParser<A>(parser: Parser<Character, A>, _ input: String) -> String {
+    var result: [String] = []
+    for (x, s) in parser.p(input.slice) {
+        result += ["Success, found \(x), remainder: \(Array(s))"]
+    }
+    return result.isEmpty ? "Parsing failed." : result.joined(separator: "\n")
+}
